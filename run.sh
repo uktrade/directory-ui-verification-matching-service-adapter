b0098debff7b57df4cd9cab8079ffb716847e7cd
konradko <konrad@korzel.com>
2018-06-13 13:13:51 +0100

    Set APP_PATH and JAVA_OPTS instead of running the command

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ VERSION=3.0.0-811
 wget https://github.com/alphagov/verify-matching-service-adapter/releases/download/$VERSION/verify-matching-service-adapter-$VERSION.zip
 unzip verify-matching-service-adapter-$VERSION.zip
 rm verify-matching-service-adapter-$VERSION.zip
-java -jar ./verify-matching-service-adapter-$VERSION/verify-matching-service-adapter.jar server config.yml
+export APP_PATH="./verify-matching-service-adapter-$VERSION/verify-matching-service-adapter.jar"
+export JAVA_OPTS="server config.yml"
